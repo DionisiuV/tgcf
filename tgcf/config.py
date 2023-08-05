aa778a05d01cfb9730af77679a34c82bcb529bf8
Valentin <valentin.dionisiu@luxon-tech.com>
2023-08-06 02:02:49 +0300

    Update config.py

--- tgcf/config.py
+++ tgcf/config.py
@@ -20,12 +20,16 @@ env_file = os.path.join(pwd, ".env")
 
 load_dotenv(env_file)
 
+
 class Destination(BaseModel):
    # pylint: disable=too-few-public-methods
     def __init__(self, dest, reply_to):
         self.dest = dest
         self.reply_to = reply_to
-        
+
+    class Config:
+        orm_mode = True
+    
 
 class Forward(BaseModel):
     """Blueprint for the forward object."""
